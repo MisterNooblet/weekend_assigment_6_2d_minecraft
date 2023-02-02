884b551089e1f33702fa9c65e8390143cea66d30
misternooblet <dragonroadsupport@gmail.com>
2023-02-03 01:58:38 +0200

    grid generator seperated

--- modules/game.js
+++ modules/game.js
@@ -1,43 +1,5 @@
+import { buildPlayerBoard } from "./gridGenerator.js"
+
 let matrixBoard = JSON.parse(localStorage.getItem('map'))
 const grid = document.querySelector('#gamegrid')
-function buildPlayerBoard() {
-    grid.style.gridTemplateRows = `repeat(${matrixBoard.length},1fr)`
-    grid.style.gridTemplateColumns = `repeat(${matrixBoard[0].length},1fr)`
-    matrixBoard.forEach(rows => {
-        rows.forEach(col => {
-            console.log(col == 'leaves');
-            let tile = document.createElement('div')
-            tile.classList.add('tile')
-            switch (col) {
-                case 'leaves':
-                    tile.classList.add('leaves')
-                    break;
-                case 'ruby':
-                    tile.classList.add('ruby')
-                    break;
-                case 'diamond':
-                    tile.classList.add('diamond')
-                    break;
-                case 'coal':
-                    tile.classList.add('coal')
-                    break;
-                case 'tree':
-                    tile.classList.add('tree')
-                    break;
-                case 'grass':
-                    tile.classList.add('grass')
-                    break;
-                case 'stone':
-                    tile.classList.add('stone')
-                    break;
-                case 'sky':
-                    tile.classList.add('sky')
-                    break;
-                default:
-                    break;
-            }
-            grid.appendChild(tile)
-        })
-    })
-}
 buildPlayerBoard()
--- modules/gridGenerator.js
+++ modules/gridGenerator.js
@@ -1,9 +1,42 @@
 let matrixBoard = JSON.parse(localStorage.getItem('map'));
-
+const grid = document.querySelector('#gamegrid')
 export function buildPlayerBoard() {
+    grid.style.gridTemplateRows = `repeat(${matrixBoard.length},1fr)`
+    grid.style.gridTemplateColumns = `repeat(${matrixBoard[0].length},1fr)`
     matrixBoard.forEach(rows => {
         rows.forEach(col => {
-            document.getElementById('#gamegrid').append("<div class= 'grid'></div>")
+            console.log(col == 'leaves');
+            let tile = document.createElement('div')
+            tile.classList.add('tile')
+            switch (col) {
+                case 'leaves':
+                    tile.classList.add('leaves')
+                    break;
+                case 'ruby':
+                    tile.classList.add('ruby')
+                    break;
+                case 'diamond':
+                    tile.classList.add('diamond')
+                    break;
+                case 'coal':
+                    tile.classList.add('coal')
+                    break;
+                case 'tree':
+                    tile.classList.add('tree')
+                    break;
+                case 'grass':
+                    tile.classList.add('grass')
+                    break;
+                case 'stone':
+                    tile.classList.add('stone')
+                    break;
+                case 'sky':
+                    tile.classList.add('sky')
+                    break;
+                default:
+                    break;
+            }
+            grid.appendChild(tile)
         })
     })
 }
