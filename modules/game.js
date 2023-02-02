2292fad13ef9ee3708d9c482fe258ca3e7d6696f
misternooblet <dragonroadsupport@gmail.com>
2023-02-03 01:50:48 +0200

    basic tile generation

--- modules/game.js
+++ modules/game.js
@@ -5,7 +5,37 @@ function buildPlayerBoard() {
     grid.style.gridTemplateColumns = `repeat(${matrixBoard[0].length},1fr)`
     matrixBoard.forEach(rows => {
         rows.forEach(col => {
+            console.log(col == 'leaves');
             let tile = document.createElement('div')
+            tile.classList.add('tile')
+            switch (col) {
+                case 'leaves':
+                    tile.classList.add('leaves')
+                    break;
+                case 'ruby':
+                    tile.classList.add('ruby')
+                    break;
+                case 'diamond':
+                    tile.classList.add('diamond')
+                    break;
+                case 'coal':
+                    tile.classList.add('coal')
+                    break;
+                case 'tree':
+                    tile.classList.add('tree')
+                    break;
+                case 'grass':
+                    tile.classList.add('grass')
+                    break;
+                case 'stone':
+                    tile.classList.add('stone')
+                    break;
+                case 'sky':
+                    tile.classList.add('sky')
+                    break;
+                default:
+                    break;
+            }
             grid.appendChild(tile)
         })
     })
