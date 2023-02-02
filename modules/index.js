2c6034fe938a78ed182691981c592bf6b708de6f
misternooblet <dragonroadsupport@gmail.com>
2023-02-02 18:50:21 +0200

    s

--- modules/index.js
+++ modules/index.js
@@ -90,10 +90,10 @@ function updateNums(side) {
 
 document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#section").style.visibility = "hidden";
         document.querySelector("#loader").style.visibility = "visible";
     } else {
         document.querySelector("#loader").style.display = "none";
-        document.querySelector("body").style.visibility = "visible";
+        document.querySelector("#section").style.visibility = "visible";
     }
 };
